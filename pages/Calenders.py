30f55ceb76a8c5b7bf08d2dac25974138c5f5d6b
Hebbelstrup <104072341+Hebbelstrup@users.noreply.github.com>
2023-05-17 13:44:03 +0200

    Added page that displays the three calenders used in SBiNlab all in one page.

--- pages/Calenders.py
+++ pages/Calenders.py
@@ -1,14 +1,6 @@
-import base64
-import io
 import dash
-import plotly.graph_objects as go
-import numpy as np
-import pandas as pd
-from scipy import optimize
-from sklearn.metrics import r2_score
-from dash import dcc,callback
 from dash import html
-from dash.dependencies import Input, Output, State
+
 
 
 

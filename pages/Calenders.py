c5752352c7964c024aa08d3023e9d69a0d1abed7
Hebbelstrup <104072341+Hebbelstrup@users.noreply.github.com>
2023-05-17 13:53:15 +0200

    Changed dimensions of calenders

--- pages/Calenders.py
+++ pages/Calenders.py
@@ -11,14 +11,14 @@ dash.register_page(__name__, title='Calenders')
 layout = html.Div(children=[html.H1('Overview of calenders for SBiNLab',style={'textAlign': 'center', 'marginTop': 40, 'marginBottom': 20}),
                             html.Iframe(
                                 src='https://teamup.com/ks2c34d39bb683096a',
-                                style={'border':'O','width':'1400px','height':'1000px'},className="d-grid gap-2 col-6 mx-auto"),
+                                style={'border':'O','width':'1850px','height':'800px'},className="d-grid gap-2 col-6 mx-auto"),
                             html.Iframe(
                                 src='https://teamup.com/ksk3sa262qd9msry66',
-                                style={'border': 'O', 'width': '1400px', 'height': '1000px'},className="d-grid gap-2 col-6 mx-auto"),
+                                style={'border': 'O', 'width': '900px', 'height': '600px','margin':26}),
                             html.Iframe(
                                 src='https://teamup.com/ksiam57om3ixp4tjwr',
-                                style={'border': 'O', 'width': '1400px', 'height': '1000px'},
-                                className="d-grid gap-2 col-6 mx-auto")
+                                style={'border': 'O', 'width': '900px', 'height': '600px','margin':26},
+                                )
 
 
 
